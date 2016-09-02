b4ce16e1c5bca53fe64aecaf4915c4d0739d31a5
Georg Walther <georgwalther@gmail.com>
2016-09-02 11:17:11 +0200

    Use more legible variable label

--- nexusadspy/tests/test_client.py
+++ nexusadspy/tests/test_client.py
@@ -31,19 +31,19 @@ def test_http_methods():
         with patch.object(AppnexusClient, "_do_authenticated_request", autospec=True) as mock_auth:
             mock_auth.return_value = (200, {})
             mock_paged.return_value = (200, {})
-            clnt = AppnexusClient("foo")
-            clnt.request("bar", "get")
+            client = AppnexusClient("foo")
+            client.request("bar", "get")
             assert mock_auth.call_count == 0
-            mock_paged.assert_called_once_with(clnt, clnt.endpoint + "/bar", "get",
+            mock_paged.assert_called_once_with(client, client.endpoint + "/bar", "get",
                                                data={}, get_field=None, headers=None, params={})
 
     with patch.object(AppnexusClient, "_do_paged_get", autospec=True) as mock_paged:
         with patch.object(AppnexusClient, "_do_authenticated_request", autospec=True) as mock_auth:
             mock_auth.return_value = (200, {})
             mock_paged.return_value = (200, {})
-            clnt = AppnexusClient("bar")
-            clnt.request("foo", "post")
-            mock_auth.assert_called_once_with(clnt, clnt.endpoint + "/foo", "post",
+            client = AppnexusClient("bar")
+            client.request("foo", "post")
+            mock_auth.assert_called_once_with(client, client.endpoint + "/foo", "post",
                                               data={}, headers=None, params={})
             assert mock_paged.call_count == 0
 
@@ -51,9 +51,9 @@ def test_http_methods():
         with patch.object(AppnexusClient, "_do_authenticated_request", autospec=True) as mock_auth:
             mock_auth.return_value = (200, {})
             mock_paged.return_value = (200, {})
-            clnt = AppnexusClient("pfoo")
-            clnt.request("pbar", "put")
-            mock_auth.assert_called_once_with(clnt, clnt.endpoint + "/pbar", "put",
+            client = AppnexusClient("pfoo")
+            client.request("pbar", "put")
+            mock_auth.assert_called_once_with(client, client.endpoint + "/pbar", "put",
                                               data={}, headers=None, params={})
             assert mock_paged.call_count == 0
 
@@ -61,16 +61,16 @@ def test_http_methods():
         with patch.object(AppnexusClient, "_do_authenticated_request", autospec=True) as mock_auth:
             mock_auth.return_value = (200, {})
             mock_paged.return_value = (200, {})
-            clnt = AppnexusClient("dfoo")
-            clnt.request("dbar", "delete")
-            mock_auth.assert_called_once_with(clnt, clnt.endpoint + "/dbar", "delete",
+            client = AppnexusClient("dfoo")
+            client.request("dbar", "delete")
+            mock_auth.assert_called_once_with(client, client.endpoint + "/dbar", "delete",
                                               data={}, headers=None, params={})
             assert mock_paged.call_count == 0
 
 
 def test_wrong_http_methods():
-    clnt = AppnexusClient("foo")
+    client = AppnexusClient("foo")
     with pytest.raises(ValueError) as excinfo:
-        clnt.request("bar", "wget")
+        client.request("bar", "wget")
         assert excinfo.value.lower() == 'Argument "method" must be one of ' \
             '["get", "post", "put", "delete"]. You supplied: "wget".'
