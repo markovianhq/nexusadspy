bc6f583fbd176c8a6b03a3899bf5594d1867d50d
alexf <alexf@markovian.com>
2017-01-19 23:53:00 +0100

    Fix upload string creator, feed order of segment fields via argument

--- nexusadspy/segment.py
+++ nexusadspy/segment.py
@@ -17,17 +17,21 @@ class AppnexusSegmentsUploader:
     BATCH_UPLOAD_ANDROID_SPECIFIER = '8'
     BATCH_UPLOAD_IOS_SPECIFIER = '3'
 
-    def __init__(self, users_list, segment_code, separators, member_id,
+    def __init__(self, users_list, segment_code, upload_string_order, separators, member_id,
                  credentials_path='.appnexus_auth.json'):
         """
         Batch-upload API wrapper for AppNexus.
         :param users_list: list, List of dictionaries representing AppNexus users. Every member should have fields
-            - uid: AppNexus user ID. AAID/IDFS in case of mobile.
+            - uid: AppNexus user ID. AAID/IDFS in case of mobile. Always first in upload string.
             - timestamp: POSIX timestamp when user entered the segment.
+            - segment_id (optional): int, Integer representing the segment id. Cannot be used at the same time as
+              segment code.
             - expiration (optional): Expiration timestamp for the user. A POSIX timestamp. Defaults to 0.
             - value (optional): Numerical value for the segment. Defaults to 0.
             - mobile_os (optional): OS used by the user. Considered internally by AppNexus to be desktop if absent.
         :param segment_code: str, Segment code to add users to.
+        :param upload_string_order: list, List specifying the order of inputs behind uid for the upload string. Possible values are
+        seg_id, timestamp, expiration, value, member_id, seg_code. Specify only one of: seg_id, seg_code
         :param separators: list, List of five field separators. As documented in
         https://wiki.appnexus.com/display/api/Batch+Segment+Service+-+File+Format#BatchSegmentService-FileFormat-Separators
         :param member_id: str, Member ID for AppNexus account.
@@ -36,6 +40,7 @@ class AppnexusSegmentsUploader:
         """
         self._credentials_path = credentials_path
         self._users_list = users_list
+        self._upload_string_order = upload_string_order
         self._segment_code = segment_code  # Appnexus bug: Segment upload batch API does not work with segment IDs
         self._separators = separators
         self._member_id = member_id
@@ -88,17 +93,12 @@ class AppnexusSegmentsUploader:
         return compressed_buffer
 
     def _get_upload_string_for_user(self, user):
-        """
-        The upload string needs to follow the order that has been specified with AppNexus:
-        uid, seg_id, timestamp, expiration, value, member_id, seg_code
-        """
+        user['member_id'] = self._member_id
+        user['seg_code'] = self._segment_code
         upload_string = str(user['uid']) + self._separators[0]
-        upload_string += '' + self._separators[2]  # Setting segment id to empty string
-        upload_string += str(user['timestamp']) + self._separators[2]
-        upload_string += str(user.get('expiration', 0)) + self._separators[2]
-        upload_string += str(user.get('value', 0)) + self._separators[2]
-        upload_string += str(self._member_id) + self._separators[2]
-        upload_string += str(self._segment_code)  # Feeding segment code
+        for item in self._upload_string_order:
+            upload_string += str(user.get(item, '0')) if not item == 'seg_id' else ''
+            upload_string += self._separators[2] if not item == self._upload_string_order[-1] else ''
         user_mobile_os = user.get('mobile_os')
         if user_mobile_os is not None:
             if user_mobile_os.lower() == 'android':
