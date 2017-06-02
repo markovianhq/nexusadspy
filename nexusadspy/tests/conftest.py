4dfae96034719877e4e0148fce91f02e687f9780
Alex <alexf@markovian.com>
2017-06-02 15:21:09 +0200

    Amend test

--- nexusadspy/tests/conftest.py
+++ nexusadspy/tests/conftest.py
@@ -6,16 +6,16 @@ def segment_batch():
 
     segment_batch = [
         {'uid': 1, 'timestamp': 1278250469, 'expiration': 48, 'value': 42,
-         'mobile_os': None, 'member_id': 7007, 'seg_id': 123, 'seg_code': '123'},
+         'type': None, 'member_id': 7007, 'seg_id': 123, 'seg_code': '123'},
         {'uid': 2, 'timestamp': 1278254459, 'expiration': 0, 'value': 0,
-         'mobile_os': 'ios', 'member_id': 7007, 'seg_id': 234, 'seg_code': '555'},
+         'type': 'idfa', 'member_id': 7007, 'seg_id': 234, 'seg_code': '555'},
         {'uid': 2, 'timestamp': 1278250469, 'expiration': 223454,
          'seg_id': 234, 'seg_code': '444'},
-        {'uid': 3, 'timestamp': 1278232469, 'expiration': -1, 'mobile_os': 'android',
+        {'uid': 3, 'timestamp': 1278232469, 'expiration': -1, 'type': 'aaid',
          'member_id': 7007, 'seg_id': 567, 'seg_code': '321'},
         {'uid': 4, 'timestamp': 1278211469, 'expiration': 12, 'value': 20,
-         'mobile_os': None, 'member_id': 7007, 'seg_id': 890, 'seg_code': '456'},
+         'type': None, 'member_id': 7007, 'seg_id': 890, 'seg_code': '456'},
         {'uid': 4, 'timestamp': 1278431469, 'expiration': 21, 'value': 10,
-         'mobile_os': None, 'member_id': 7007, 'seg_id': 890, 'seg_code': '777'}
+         'type': None, 'member_id': 7007, 'seg_id': 890, 'seg_code': '777'}
     ]
     return segment_batch
